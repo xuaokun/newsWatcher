7c313cc9a291aa255b4d2b2f361a7d105e1df4b1
xuaokun <xuaokun1997@163.com>
2021-11-13 11:04:42 +0800

    feat:负面新闻预警

--- src/logic/news-watcher/negative-trend.js
+++ src/logic/news-watcher/negative-trend.js
@@ -2,7 +2,7 @@
  * @Description: 负面新闻趋势
  * @Author: akxu
  * @Date: 2021-11-04 17:40:16
- * @LastEditTime: 2021-11-04 17:44:57
+ * @LastEditTime: 2021-11-12 17:43:22
  * @LastEditors: AKXU-NB1
  * @LastEditContent:
  */
@@ -33,4 +33,28 @@ function getNegativeTrend(companyName) {
     });
 }
 
-export { getNegativeTrend };
+//获取负面新闻预警
+function getNegativeNews(companyName) {
+  return apiService
+    .query("/api/newsWatcher/getMyCompanyNews", {
+      params: {
+        companyName
+      }
+    })
+    .then(res => {
+      if (res.data.status === 0) {
+        return res.data.msg;
+      } else {
+        throw new Error(`处理参数异常${res.data.info}`);
+      }
+    })
+    .catch(e => {
+      store.dispatch("snackbar/openSnackbar", {
+        msg: `查询出错：${e}`,
+        color: "error"
+      });
+      return {};
+    });
+}
+
+export { getNegativeTrend, getNegativeNews };
--- src/view/news-watcher/negative-trend/index.vue
+++ src/view/news-watcher/negative-trend/index.vue
@@ -2,19 +2,27 @@
  * @Description: 负面舆情走势
  * @Author: akxu
  * @Date: 2021-11-04 13:32:46
- * @LastEditTime: 2021-11-04 22:20:24
+ * @LastEditTime: 2021-11-13 10:36:51
  * @LastEditors: AKXU-NB1
  * @LastEditContent: 
 -->
 <template>
   <div>
     <v-row>
-      <v-col cols="12">
+      <v-col cols="6">
         <negative-trend
           title="负面新闻趋势"
           :dataList="trendList"
           :nameList="trendDates"
           @searchTrend="searchTrendByDate"
+        />
+      </v-col>
+      <v-col cols="6">
+        <publish-list
+          tableTitle="负面新闻预警"
+          :tableHead="tableHead"
+          :dataList="tableData"
+          :pageLength="pageLength"
         />
       </v-col>
     </v-row>
@@ -23,17 +31,51 @@
 
 <script>
 import NegativeTrend from "@/components/SearchNameLineChart/index.vue";
+import PublishList from "@/components/PublishList/index.vue";
 import { SET_BREADCRUMB } from "@/core/services/store/breadcrumbs.module";
-import { getNegativeTrend } from "@/logic/news-watcher/negative-trend";
+import {
+  getNegativeTrend,
+  getNegativeNews
+} from "@/logic/news-watcher/negative-trend";
 export default {
   components: {
-    NegativeTrend
+    NegativeTrend,
+    PublishList
   },
   data() {
     return {
       trendList: [],
       trendDates: [],
-      searchTrend: []
+      searchTrend: [],
+      tableHead: [
+        {
+          name: "时间",
+          property: "pubdate",
+          time: true
+        },
+        {
+          name: "新闻标题",
+          property: "title",
+          maxLen: 30
+        },
+        {
+          name: "操作",
+          property: "oper",
+          sortAble: false,
+          currentSort: -1 //0代表升序
+        }
+      ],
+      tableData: [
+        {
+          pubdate: 1,
+          title: "小动作不断！美国海军两艘濒海战斗舰被曝近日频繁进出南海"
+        },
+        {
+          pubdate: 2,
+          title: "最新！海地7.2级地震已造成至少724人死亡"
+        }
+      ],
+      pageLength: 1
     };
   },
   methods: {
@@ -52,6 +94,10 @@ export default {
           return item._id;
         });
       });
+
+      getNegativeNews(params).then(re => {
+        this.tableData = re.data;
+      });
     }
   },
   mounted() {
@@ -61,20 +107,21 @@ export default {
     ]);
   },
   created() {
-    getNegativeTrend("美国").then(re => {
-      const trendList = re.data.map(item => {
-        return item.count;
-      });
-      this.trendList = [
-        {
-          name: "美国",
-          data: trendList
-        }
-      ];
-      this.trendDates = re.data.map(item => {
-        return item._id;
-      });
-    });
+    // getNegativeTrend("美国").then(re => {
+    //   const trendList = re.data.map(item => {
+    //     return item.count;
+    //   });
+    //   this.trendList = [
+    //     {
+    //       name: "美国",
+    //       data: trendList
+    //     }
+    //   ];
+    //   this.trendDates = re.data.map(item => {
+    //     return item._id;
+    //   });
+    // });
+    this.searchTrendByDate("美国");
   }
 };
 </script>
